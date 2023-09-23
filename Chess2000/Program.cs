0dd054bafd4b728a16501009cd6d5b7b7b5e63eb
shinpansen <shinpansen@gmail.com>
2023-09-23 19:35:11 +0200

    test git

--- Chess2000/Program.cs
+++ Chess2000/Program.cs
@@ -5,6 +5,7 @@ using Chess2000.BoardGame.Rules.Chess;
 ChessPiece p1 = new BlackPawn();
 ChessPiece p2 = new BlackPawn();
 var test = p1.IsFriend(new ChessPieceVisitor(p2));
+//Test
 
 using var game = new Chess2000.MyGame();
 game.Run();
